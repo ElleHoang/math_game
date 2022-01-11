b88d6b47f0fa96c2d5f7934bb154bbe00671e5ce
Linh Hoang <ellehoang@hotmail.com>
2022-01-11 08:58:51 -0500

    iniital commit

---
+++ game.rb
@@ -0,0 +1,27 @@
+class MathGame
+
+  def initialize
+    # 2 players
+    # current player
+    # start the game
+  end
+
+  def new_turn
+    # Game ask question
+    # solultion to question
+    # 2 numbers btwn 1...20
+    # player answer question
+  end
+
+  def end_turn
+    # output new scores for noth player
+    # if current player change to next player
+  end
+
+  def end_game
+    # end when one player loses all their lives
+    # announce who won and their score
+    # Good bye message
+  end
+
+end
---
+++ player.rb
@@ -0,0 +1,16 @@
+class Players
+  
+  def initialize
+    # player name
+    # players have 3 lives each
+  end
+
+  def lose_life
+    # if mis-answer question - 1 life
+  end
+
+  def dead?
+    # when lives = 0
+  end
+  
+end
